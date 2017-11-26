df300b0b1d8fd10f75285ba507b694c74b4ad54e
Pavel <triodjangopiter@yandex.ru>
2017-11-26 16:28:11 +0300

    complete db structure

---
+++ src/main/java/ru/javavision/App.java
@@ -0,0 +1,12 @@
+package ru.javavision;
+
+/**
+ * Author : Pavel Ravvich.
+ * Created : 26/11/2017.
+ * <p>
+ * App
+ */
+public class App {
+
+
+}
--- src/main/java/ru/javavision/dao/DAODirector.java
+++ src/main/java/ru/javavision/dao/DAODirector.java
@@ -1,5 +1,9 @@
 package ru.javavision.dao;
 
+import com.sun.istack.internal.NotNull;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import ru.javavision.model.Director;
 
 /**
@@ -7,20 +11,72 @@ import ru.javavision.model.Director;
  * Created : 26/11/2017.
  */
 public class DAODirector implements DAO<Director, Integer> {
+    /**
+     * Gate to database.
+     */
+    private final SessionFactory sessionFactory;
 
-    public void create(Director director) {
-
+    /**
+     * Default constructor.
+     *
+     * @param sessionFactory connection gate to database.
+     */
+    public DAODirector(@NotNull final SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
     }
 
-    public void update(Director director) {
-
+    /**
+     * Create new director row.
+     *
+     * @param director for add to table director.
+     */
+    public void create(@NotNull final Director director) {
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.save(director);
+            final Transaction transaction = session.getTransaction();
+            transaction.commit();
+        }
     }
 
-    public void delete(Director director) {
-
+    /**
+     * Get director by id.
+     *
+     * @param id of director for get.
+     * @return director with param id.
+     */
+    public Director read(@NotNull final Integer id) {
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            return session.get(Director.class, id);
+        }
     }
 
-    public Director read(java.lang.Integer integer) {
-        return null;
+    /**
+     * Update existed director state by id.
+     *
+     * @param director new state for director obj.
+     */
+    public void update(@NotNull final Director director) {
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.update(director);
+            final Transaction transaction = session.getTransaction();
+            transaction.commit();
+        }
+    }
+
+    /**
+     * Delete director.
+     *
+     * @param director for delete.
+     */
+    public void delete(@NotNull final Director director) {
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.delete(director);
+            final Transaction transaction = session.getTransaction();
+            transaction.commit();
+        }
     }
 }
