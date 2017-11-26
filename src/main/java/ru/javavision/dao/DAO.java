0794f119b1d8d5f3551dd006f730edd4aef0c21a
Pavel <triodjangopiter@yandex.ru>
2017-11-26 21:47:21 +0300

    complete db structure

--- src/main/java/ru/javavision/dao/DAO.java
+++
@@ -1,9 +0,0 @@
-package ru.javavision.dao;
-
-public interface DAO<Entity, Key> {
-
-    void create(Entity entity);
-    Entity read(Key key);
-    void update(Entity entity);
-    void delete(Entity entity);
-}
