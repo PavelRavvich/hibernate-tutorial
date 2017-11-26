80ba3dd28febc84f4907574ec0415fb0ee2cb76e
Pavel <triodjangopiter@yandex.ru>
2017-11-27 01:37:00 +0300

    complete config lesson

--- db/db.sql
+++ db/db.sql
@@ -9,15 +9,3 @@ CREATE TABLE IF NOT EXISTS engines (
 
 INSERT INTO engines (id, model, power) VALUES (DEFAULT, 'engine_model_01', 1250);
 INSERT INTO engines (id, model, power) VALUES (DEFAULT, 'engine_model_02', 2820);
-
-
-CREATE TABLE IF NOT EXISTS cars (
-  id       SERIAL PRIMARY KEY,
-  mark     VARCHAR(25) NOT NULL,
-  model    VARCHAR(25) NOT NULL,
-  engin_id INTEGER     NOT NULL,
-  FOREIGN KEY (engin_id) REFERENCES engines (id)
-);
-
-INSERT INTO cars (id, mark, model, engin_id) VALUES (DEFAULT, 'car_mark_01', 'car_model_01', 1);
-INSERT INTO cars (id, mark, model, engin_id) VALUES (DEFAULT, 'car_mark_02', 'car_model_02', 2);
