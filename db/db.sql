3a6eb7e46b30b0bbdaf0c3b91edbd55ce0facb89
Pavel <triodjangopiter@yandex.ru>
2017-11-27 16:02:40 +0300

    complete

--- db/db.sql
+++ db/db.sql
@@ -1,10 +1,9 @@
 CREATE DATABASE config_lesson ENCODING 'UTF-8';
 
 CREATE TABLE IF NOT EXISTS engines (
-  id    SERIAL UNIQUE      PRIMARY KEY,
-  model VARCHAR(25) NOT NULL,
+  model VARCHAR(25) PRIMARY KEY,
   power INTEGER     NOT NULL
 );
 
-INSERT INTO engines (id, model, power) VALUES (DEFAULT, 'engine_model_01', 1250);
-INSERT INTO engines (id, model, power) VALUES (DEFAULT, 'engine_model_02', 2820);
+INSERT INTO engines (model, power) VALUES ('engine_model_01', 1250);
+INSERT INTO engines (model, power) VALUES ('engine_model_02', 2820);
